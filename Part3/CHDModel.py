64fdab96cdd2f37ea8c902f4faf7e2feecb4fd71
Alec McAllister <>
2019-12-06 21:28:17 -0700

    Implemented a simple model for heart_train.csv that reports ~70% accuracy.

--- Part3/CHDModel.py
+++ Part3/CHDModel.py
@@ -1,13 +1,50 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import functools
-
 import numpy as np
 import tensorflow as tf
+import pandas as pd
+from tensorflow import keras
+
+train_file_path = 'heart_train.csv'  # 396 rows
+test_file_path = 'heart_test.csv'  # 66 rows
 
 
-#train_file_path = tf.keras.utils.get_file("train.csv", 'heart.csv')
-test_file_path = 'heart.csv'
+def prepare_dataset(path):
+    df = pd.read_csv(path)
+    df['famhist'] = pd.Categorical(df['famhist'])
+    df['famhist'] = df.famhist.cat.codes
+    df.pop('row.names')
 
-np.set_printoptions(precision=3, suppress=True)
+    target = df.pop('chd')
+    dataset = tf.data.Dataset.from_tensor_slices((df.values, target.values))
+    shuffled_dataset = dataset.shuffle(len(df)).batch(1).repeat()
 
-print(test_file_path)
+    return shuffled_dataset, df
+
+
+train_dataset, train_df = prepare_dataset(train_file_path)
+test_dataset, test_df = prepare_dataset(test_file_path)
+
+
+def get_compiled_model():
+    model = keras.Sequential([
+        keras.layers.Dense(9, activation='relu'),
+        keras.layers.Dense(20, activation='relu'),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10, activation='relu'),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+model = get_compiled_model()
+model.fit(train_dataset, epochs=7, steps_per_epoch=train_df.shape[0])
+
+model_loss, model_acc = model.evaluate(test_dataset, steps=test_df.shape[0],  verbose=2)
+print(f"Model Loss:     {model_loss:.2f}")
+print(f"Model Accuracy: {model_acc*100:.1f}%")
