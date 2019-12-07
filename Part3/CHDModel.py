a0693558fa1cf7a0bf2d081b8da4e48bdc68e412
Alec McAllister <>
2019-12-06 19:51:21 -0700

    Split heart.csv into heart_train.csv and heart_test.csv.

--- Part3/CHDModel.py
+++ Part3/CHDModel.py
@@ -5,3 +5,9 @@ import numpy as np
 import tensorflow as tf
 
 
+#train_file_path = tf.keras.utils.get_file("train.csv", 'heart.csv')
+test_file_path = 'heart.csv'
+
+np.set_printoptions(precision=3, suppress=True)
+
+print(test_file_path)
